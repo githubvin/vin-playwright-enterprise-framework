6f6057c472c91106858816445d4187f2bd3c2df4
unknown <vinothksc@gmail.com>
2025-01-22 14:58:00 +0530

    CSV and Json files added for data and Utils added for CSV to Json

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -2,12 +2,13 @@
 
 import {expect, Page} from "@playwright/test"; 
 import logger from "../utils/LoggerUtil";
-
+import ContactsPage from "./ContactsPage";
 
 
 export default class HomePage {
 
     private readonly serviceTitleLocator = "Service"; 
+    private readonly contactsLinkLocator = "Contacts"; 
 
     constructor(private page: Page) {
         // no need to mention as this.page 
@@ -19,6 +20,16 @@ export default class HomePage {
             logger.error(`Error clicking login button: ${error}`); 
             throw error; // rethrow error if needed 
         }).then(() => logger.info("Service Title is visible")); 
+    } 
+
+    async navigateToContactsTab() {
+
+        await expect(this.page.getByRole('link', {name: this.contactsLinkLocator})).toBeVisible(); 
+        logger.info("Contacts Tab is visible"); 
+        await this.page.getByRole('link', {name: this.contactsLinkLocator}).click(); 
+        logger.info("Contacts Tab is clicked"); 
+        return new ContactsPage(this.page); 
+
     }
 
 }
---
+++ src/tests/contactTest.spec.ts
@@ -0,0 +1,72 @@
+import { test } from "@playwright/test";
+import { decrypt } from "../utils/CryptojsUtil";
+import logger from "../utils/LoggerUtil";
+// import cdata from "../data/contacts.json"; 
+import cdata from "../data/contactsdatademo.json"; 
+import { convertCsvFileToJsonFile } from "../utils/CsvtoJsonUtil"; 
+// import { exportToCsv, exportToJson, generateTestData } from "../utils/FakerDataUtil";
+// import { demoOutput } from "../utils/fakersample";
+import LoginPage from "../pages/LoginPage";
+
+for (const contact of cdata) {
+  test(`Advance DD test for ${contact.firstName} `, async ({ page }) => {
+    logger.info("Test for Contact Creation is started...");
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateToLoginPage();
+    await loginPage.fillUsername(decrypt(process.env.userid!));
+    await loginPage.fillPassword(decrypt(process.env.password!));
+    const homePage = await loginPage.clickLoginButton();
+    await homePage.expectServiceTitleToBeVisible();
+    const contactsPage = await homePage.navigateToContactsTab();
+    await contactsPage.createNewContact(contact.firstName, contact.lastName);
+    await contactsPage.expectContactLabelContainsFirstNameAndLastName(
+      contact.firstName,
+      contact.lastName
+    );
+    logger.info("Test for Contact Creation is completed");
+  });
+}
+
+test.skip("simple DD test", async ({ page }) => {
+  logger.info("Test for Contact Creation is started...");
+  const fname = "Shiva";
+  const lname = "Rudra";
+  const loginPage = new LoginPage(page);
+  await loginPage.navigateToLoginPage();
+  await loginPage.fillUsername(decrypt(process.env.userid!));
+  await loginPage.fillPassword(decrypt(process.env.password!));
+  const homePage = await loginPage.clickLoginButton();
+  // await homePage.expectServiceTitleToBeVisible();
+  const contactsPage = await homePage.navigateToContactsTab();
+  await contactsPage.createNewContact(fname, lname);
+  await contactsPage.expectContactLabelContainsFirstNameAndLastName(
+    fname,
+    lname
+  );
+  logger.info("Test for Contact Creation is completed");
+});
+
+// this test is executed only to convert the CSV file to JSON - no need to run always 
+test.skip("csv to json", async () => {
+  convertCsvFileToJsonFile("contactsdata.csv", "contactsdatademo.json");
+});
+
+
+test.skip("demo faker", async () => { 
+
+  console.log(demoOutput)
+
+ });
+
+test.skip("Faker", async ({ page }) => { 
+
+  // Generate test data
+const testData = generateTestData(20);
+
+// Export data to JSON file
+exportToJson(testData, 'testData_en.json');
+
+// Export data to CSV file
+exportToCsv(testData, 'testData_en.csv');
+
+ });
