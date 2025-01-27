3dfbe949bfebf28f7cf9144ae710a24eeda0a794
unknown <vinothksc@gmail.com>
2025-01-27 12:38:03 +0530

    Added storage state login test example

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,12 +1,14 @@
 
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import LoginPage from "../pages/LoginPage"; 
 import logger from "../utils/LoggerUtil";
 import { encrypt, decrypt } from "../utils/CryptojsUtil"; 
 // import { encryptEnvFile, decryptEnvFile } from "../utils/EncryptenvFile"; 
 
+// declaring variable to store the authentication with storage context 
+const authFile = "src/config/auth.json"; 
 
-test(`simple login test`, async({page}) => {
+test.skip(`simple login test`, async({page}) => {
     
     const loginPage = new LoginPage(page); 
 
@@ -23,7 +25,46 @@ test(`simple login test`, async({page}) => {
     await homePage.expectServiceTitleToBeVisible(); 
     logger.info("Test for login is completed"); 
 
+    // using storage state we are going to save this context 
+    await page.context().storageState({path: authFile}); 
 }) 
+
+
+test.skip(`Login and store the context example test`, async({page}) => {
+
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); 
+    await page.locator("[name='username']").fill("Admin"); 
+    await page.getByPlaceholder("Password").fill("admin123"); 
+    await page.getByRole('button', "Login").click(); 
+
+    await page.context().storageState({path: "src/config/orangehrmauth.json"}); 
+    // Even this auth file details may expire it is not recommended to push the auth files into repository for security reasons 
+
+})
+
+
+test(`Login with orange hrm auth file `, async({browser}) => {
+
+    const context = await browser.newContext({storageState: "src/config/orangehrmauth.json"}); 
+    const page = await context.newPage(); 
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"); 
+
+    // validating text in the dashboard 
+    await expect(page.getByText("Time at Work")).toBeVisible({timeout: 4000}); 
+
+    // we can store the login context by command line also 
+    // npx playwright codegen <application URL> --save-storage=<path/auth.json>  
+
+    // then using the same auth file here 
+    // npx playwright codegen --load-storage=<path/auth.json> <app URL>
+
+})
+
+
+// another way to use the storage state 
+// test.use({
+//     storageState: "src/config/orangehrmauth.json"
+// })
 
 
 // this test is used to call the encryption function to encrypt env content 
