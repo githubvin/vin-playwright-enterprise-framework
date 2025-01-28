b12f0265621d50a841d229a7149d9045d1b438e5
unknown <vinothksc@gmail.com>
2025-01-28 21:43:45 +0530

    Visualization Tests added

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000', 
-    baseURL: "https://reqres.in", 
+    // baseURL: "https://reqres.in", 
+    baseURL: "https://login.salesforce.com",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry', 
---
+++ src/tests/visTest.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from "@playwright/test";
+import LoginPage from "../pages/LoginPage";
+
+test("Verify Logo Placement and Size", async ({ page }) => {
+  await page.goto("/");
+  const logo = await page.getByAltText('Salesforce');
+  const boundingBox = await logo?.boundingBox();
+  if (boundingBox) {
+    expect(boundingBox.width).toBe(160.890625);
+    expect(boundingBox.height).toBe(112.984375);
+    
+  }
+});
+
+test("Confirm logo Color", async ({ page }) => {
+    await page.goto("/");
+    const logo = await page.getByAltText('Salesforce');
+
+  // Get the computed style of the button
+  const logoStyle = await logo.evaluate((element) => {
+    const style = window.getComputedStyle(element);
+    return {
+      color: style.color,
+      // Add other styles you want to validate here
+    };
+  });
+  // Assert the background color of the button
+  expect(logoStyle.color).toBe("rgb(22, 50, 92)"); 
+//   expect(logoStyle.color).toBe("rgb(232,233,22)")
+  // Replace with your expected background color
+});
+
+
+test('Screenshot compare test', async ({ page}) => {
+  await page.goto('/');
+  const loginPage = new LoginPage(page);
+  await loginPage.fillUsername("demo");
+//   await page.screenshot({path: "screen.png"})
+  await expect(page).toHaveScreenshot();
+});
